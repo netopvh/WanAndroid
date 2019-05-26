9075f4533161f9b8d7fcdc6dfd7a3859cdb35fad
suming <whamu22@163.com>
2019-05-26 23:55:55 +0800

    侧滑

--- app/src/main/java/com/whamu2/wanandroid/mvp/ui/activity/MainActivity.java
+++ app/src/main/java/com/whamu2/wanandroid/mvp/ui/activity/MainActivity.java
@@ -1,11 +1,18 @@
 package com.whamu2.wanandroid.mvp.ui.activity;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.whamu2.wanandroid.R;
@@ -17,6 +24,8 @@ import com.whamu2.wanandroid.mvp.ui.fragment.SystemFragment;
 import com.whamu2.wanandroid.mvp.ui.fragment.TopArticleFragment;
 import com.whamu2.wanandroid.mvp.ui.fragment.WechatFragment;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,8 +34,14 @@ import java.util.List;
  * @date 2019/3/7
  * @address https://github.com/whamu2
  */
-public class MainActivity extends BaseDataBindingActivity<ActivityMainBinding> {
+public class MainActivity extends BaseDataBindingActivity<ActivityMainBinding>  {
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private ViewPager mViewPager;
+    private BottomNavigationView mBottomNavigationView;
 
     @Override
     public int initView() {
@@ -35,36 +50,97 @@ public class MainActivity extends BaseDataBindingActivity<ActivityMainBinding> {
 
     @Override
     public void initData(@Nullable Bundle savedInstanceState) {
+        mToolbar = findViewById(R.id.toolbar);
+        mViewPager = findViewById(R.id.viewpager);
+        mBottomNavigationView = findViewById(R.id.navigation);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mNavigationView = findViewById(R.id.nav_view);
+
+        setSupportActionBar(mToolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        mNavigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
+
         Adapter adapter = new Adapter(getSupportFragmentManager());
         adapter.addFragment(TopArticleFragment.newInstance());
         adapter.addFragment(SystemFragment.newInstance());
         adapter.addFragment(ProjectFragment.newInstance());
         adapter.addFragment(WechatFragment.newInstance());
         adapter.addFragment(MineFragment.newInstance());
-        mViewBinding.viewpager.setOffscreenPageLimit(4);
-        mViewBinding.viewpager.setAdapter(adapter);
+        mViewPager.setOffscreenPageLimit(4);
+        mViewPager.setAdapter(adapter);
 
-        mViewBinding.navigation.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mViewPager.setCurrentItem(0);
+                    break;
+                case R.id.navigation_system:
+                    mViewPager.setCurrentItem(1);
+                    break;
+                case R.id.navigation_project:
+                    mViewPager.setCurrentItem(2);
+                    break;
+                case R.id.navigation_publicnumber:
+                    mViewPager.setCurrentItem(3);
+                    break;
+            }
+            return true;
+        });
     }
 
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.navigation_home:
-                mViewBinding.viewpager.setCurrentItem(0);
-                break;
-            case R.id.navigation_system:
-                mViewBinding.viewpager.setCurrentItem(1);
-                break;
-            case R.id.navigation_project:
-                mViewBinding.viewpager.setCurrentItem(2);
-                break;
-            case R.id.navigation_publicnumber:
-                mViewBinding.viewpager.setCurrentItem(3);
-                break;
-            case R.id.navigation_notifications:
-                mViewBinding.viewpager.setCurrentItem(4);
-                break;
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onNavigationItemSelected(@NotNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_tools) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 
