a9682131df6256439b43dde085a1ee7d89f8dbde
suming <whamu22@163.com>
2019-05-27 14:40:25 +0800

    nav

--- app/src/main/java/com/whamu2/wanandroid/mvp/ui/activity/MainActivity.java
+++ app/src/main/java/com/whamu2/wanandroid/mvp/ui/activity/MainActivity.java
@@ -10,19 +10,28 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.ToxicBakery.viewpager.transforms.DepthPageTransformer;
+import com.android.lib.aspect.CheckLogin;
 import com.whamu2.wanandroid.R;
 import com.whamu2.wanandroid.base.BaseDataBindingActivity;
+import com.whamu2.wanandroid.common.event.EventObj;
 import com.whamu2.wanandroid.databinding.ActivityMainBinding;
+import com.whamu2.wanandroid.mvp.model.bean.User;
 import com.whamu2.wanandroid.mvp.ui.adapter.TabAdapter;
 import com.whamu2.wanandroid.mvp.ui.fragment.MineFragment;
 import com.whamu2.wanandroid.mvp.ui.fragment.ProjectFragment;
 import com.whamu2.wanandroid.mvp.ui.fragment.SystemFragment;
 import com.whamu2.wanandroid.mvp.ui.fragment.TopArticleFragment;
 import com.whamu2.wanandroid.mvp.ui.fragment.WechatFragment;
+import com.whamu2.wanandroid.utils.database.DatabaseManager;
 
 import org.jetbrains.annotations.NotNull;
+
+import static com.whamu2.wanandroid.common.Container.Event.LOGIN;
 
 /**
  * @author Suming
@@ -35,6 +44,10 @@ public class MainActivity extends BaseDataBindingActivity<ActivityMainBinding> {
     private Toolbar mToolbar;
     private DrawerLayout mDrawerLayout;
     private ViewPager mViewPager;
+    private NavigationView mNavigationView;
+    private View mHeaderView;
+    private BottomNavigationView mBottomNavigationView;
+    private TextView nameTextView;
 
     @Override
     public int initView() {
@@ -45,9 +58,9 @@ public class MainActivity extends BaseDataBindingActivity<ActivityMainBinding> {
     public void initData(@Nullable Bundle savedInstanceState) {
         mToolbar = findViewById(R.id.toolbar);
         mViewPager = findViewById(R.id.viewpager);
-        BottomNavigationView mBottomNavigationView = findViewById(R.id.navigation);
+        mBottomNavigationView = findViewById(R.id.navigation);
         mDrawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView mNavigationView = findViewById(R.id.nav_view);
+        mNavigationView = findViewById(R.id.nav_view);
 
         /**
          * 侧栏导航设置
@@ -57,10 +70,16 @@ public class MainActivity extends BaseDataBindingActivity<ActivityMainBinding> {
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         mNavigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
+        mHeaderView = mNavigationView.getHeaderView(0);
+        handlerHeaderView();
 
-        /**
-         * 底部导航设置
-         */
+        handlerBottomNavigation();
+    }
+
+    /**
+     * 底部导航设置
+     */
+    private void handlerBottomNavigation() {
         TabAdapter adapter = new TabAdapter(getSupportFragmentManager());
         adapter.addFragment(TopArticleFragment.newInstance());
         adapter.addFragment(SystemFragment.newInstance());
@@ -93,6 +112,42 @@ public class MainActivity extends BaseDataBindingActivity<ActivityMainBinding> {
         });
     }
 
+    /**
+     * 头部设置
+     */
+    private void handlerHeaderView() {
+        ImageView headerImageView = mHeaderView.findViewById(R.id.headerImageView);
+        nameTextView = mHeaderView.findViewById(R.id.nameTextView);
+        User user = DatabaseManager.getInstance().getUser();
+        if (user == null) {
+            headerImageView.setOnClickListener(v -> onCheckLogin(R.id.imageView));
+        } else {
+            nameTextView.setText(user.getUsername());
+        }
+    }
+
+    @CheckLogin
+    public void onCheckLogin(int id) {
+        setupOwner();
+        if (id == R.id.headerImageView) {
+        }
+    }
+
+    private void setupOwner() {
+        User user = DatabaseManager.getInstance().getUser();
+        if (user != null) {
+            nameTextView.setText(user.getUsername());
+        }
+    }
+
+    @Override
+    protected void onEventSubscribe(EventObj obj) {
+        super.onEventSubscribe(obj);
+        if (obj.getKey() == LOGIN) {
+            setupOwner();
+        }
+    }
+
     @Override
     public void onBackPressed() {
         if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
@@ -109,8 +164,8 @@ public class MainActivity extends BaseDataBindingActivity<ActivityMainBinding> {
 
         if (id == R.id.nav_favorites) {
             // Handle the camera action
+            onCheckLogin(R.id.nav_favorites);
         } else if (id == R.id.nav_theme_mode) {
-
 
         } else if (id == R.id.nav_share) {
 
